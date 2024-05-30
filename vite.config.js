4cfa2448894324c52cd2573f0fcefbfa224cc1ea
G2309 <cru22779@uvg.edu.gt>
2024-05-30 13:13:12 -0600

    Build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://G2309.github.io/Portfolio'
+  base:'https://G2309.github.io/Portfolio',
 })
